80e0b3660fe72bb5c1d7dc49d9a5331c44242d85
Kylie <brooke148410@gmail.com>
2025-08-08 12:52:16 -0400

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
